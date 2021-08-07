9029d3dac59ccb8cb406f7c44d7bc30a63cb9772
John Spurlock <john@skymethod.com>
2021-08-07 11:43:13 -0500

    Start in-memory storage implementation.

--- in_process_durable_objects.ts
+++ in_process_durable_objects.ts
@@ -1,4 +1,4 @@
-import { DurableObjectNamespace, DurableObjectId, DurableObjectStub, DurableObjectState, DurableObjectStorage } from './deps_cf.ts';
+import { DurableObjectNamespace, DurableObjectId, DurableObjectStub, DurableObjectState, DurableObjectStorage, DurableObjectStorageValue, DurableObjectStorageReadOptions, DurableObjectStorageWriteOptions, DurableObjectStorageTransaction, DurableObjectStorageListOptions } from './deps_cf.ts';
 import { Bytes } from './bytes.ts';
 import { UnimplementedDurableObjectNamespace } from './unimplemented_cloudflare_stubs.ts';
 
@@ -37,7 +37,8 @@ export class InProcessDurableObjects {
             if (existing) return existing;
         }
         const ctor = this.findConstructorForClassName(className);
-        const state: DurableObjectState = new LocalDurableObjectState();
+        const storage = new InMemoryDurableObjectStorage();
+        const state: DurableObjectState = new LocalDurableObjectState(id, storage);
         const durableObject = new ctor(state, this.moduleWorkerEnv);
         if (classObjects === undefined) {
             classObjects = new Map();
@@ -119,12 +120,110 @@ class LocalDurableObjectId implements DurableObjectId {
 }
 
 class LocalDurableObjectState implements DurableObjectState {
-    get id(): DurableObjectId { throw new Error(`LocalDurableObjectState.id not implemented.`); }
-    get storage(): DurableObjectStorage { throw new Error(`LocalDurableObjectState.storage not implemented.`); }
+    readonly id: DurableObjectId;
+    readonly storage: DurableObjectStorage;
+
+    constructor(id: DurableObjectId, storage: DurableObjectStorage) {
+        this.id = id;
+        this.storage = storage;
+    }
+
     waitUntil(_promise: Promise<unknown>): void {
         throw new Error(`LocalDurableObjectState.waitUntil() not implemented.`);
     }
+
     blockConcurrencyWhile<T>(_fn: () => Promise<T>): Promise<T> {
         throw new Error(`LocalDurableObjectState.blockConcurrencyWhile() not implemented.`);
     }
+
+}
+
+class InMemoryDurableObjectStorage implements DurableObjectStorage {
+
+    private readonly sortedKeys: string[] = [];
+    private readonly values = new Map<string, DurableObjectStorageValue>();
+
+    transaction<T>(closure: (txn: DurableObjectStorageTransaction) => T): Promise<T> {
+        throw new Error(`InMemoryDurableObjectStorage.transaction not implemented`);
+    }
+
+    deleteAll(): Promise<void> {
+        this.sortedKeys.splice(0);
+        this.values.clear();
+        return Promise.resolve();
+    }
+
+    get(key: string, opts?: DurableObjectStorageReadOptions): Promise<DurableObjectStorageValue | undefined>;
+    get(keys: readonly string[], opts?: DurableObjectStorageReadOptions): Promise<Map<string, DurableObjectStorageValue>>;
+    get(keyOrKeys: string | readonly string[], opts?: DurableObjectStorageWriteOptions): Promise<Map<string, DurableObjectStorageValue> | DurableObjectStorageValue | undefined> {
+        throw new Error(`InMemoryDurableObjectStorage.get not implemented`);
+    }
+   
+    put(key: string, value: DurableObjectStorageValue, opts?: DurableObjectStorageWriteOptions): Promise<void>;
+    put(entries: Record<string, unknown>, opts?: DurableObjectStorageWriteOptions): Promise<void>;
+    put(arg1: unknown, arg2?: unknown, arg3?: unknown): Promise<void> {
+        if (typeof arg1 === 'object' && arg2 === undefined && arg3 === undefined) {
+            const entries = arg1 as Record<string, unknown>;
+            let sortedKeysChanged = false;
+            for (const [key, value] of Object.entries(entries)) {
+                if (!this.sortedKeys.includes(key)) {
+                    this.sortedKeys.push(key);
+                    sortedKeysChanged = true;
+                }
+                const val = value as DurableObjectStorageValue;
+                this.values.set(key, structuredClone(val));
+            }
+            if (sortedKeysChanged) {
+                this.sortedKeys.sort();
+            }
+            return Promise.resolve();
+        }
+        throw new Error(`InMemoryDurableObjectStorage.put not implemented arg1=${arg1}`);
+    }
+   
+    delete(key: string, opts?: DurableObjectStorageWriteOptions): Promise<boolean>;
+    delete(keys: readonly string[], opts?: DurableObjectStorageWriteOptions): Promise<number>;
+    delete(keyOrKeys: string | readonly string[], opts?: DurableObjectStorageWriteOptions): Promise<boolean | number> {
+        throw new Error(`InMemoryDurableObjectStorage.delete not implemented`);
+    }
+   
+    list(options: DurableObjectStorageListOptions & DurableObjectStorageReadOptions = {}): Promise<Map<string, DurableObjectStorageValue>> {
+        if (options.allowConcurrency === undefined && options.end === undefined && options.noCache === undefined && options.reverse === undefined && options.start === undefined) {
+            const { prefix, limit } = options;
+            const { sortedKeys, values } = this;
+            const rt = new Map<string, DurableObjectStorageValue>();
+            for (const key of sortedKeys) {
+                if (limit !== undefined && rt.size >= limit) return Promise.resolve(rt);
+                if (prefix !== undefined && !key.startsWith(prefix)) continue;
+                const value = structuredClone(values.get(key)!);
+                rt.set(key, value);
+            }
+            return Promise.resolve(rt);
+        }
+        throw new Error(`InMemoryDurableObjectStorage.list not implemented options=${JSON.stringify(options)}`);
+    }
+
+}
+
+function structuredClone<T>(value: T): T {
+    if (typeof value === 'string') return value;
+    if (typeof value === 'object') {
+        if (!isJsonSafe(value)) throw new Error(`structuredClone: object value is not json-safe: ${value}`);
+        return JSON.parse(JSON.stringify(value));
+    }
+    throw new Error(`structuredClone not implemented for ${typeof value} ${value}`);
+}
+
+function isJsonSafe(value: unknown): boolean {
+    if (value === undefined) return true;
+    if (value === null) return true;
+    if (typeof value === 'boolean') return true;
+    if (typeof value === 'string') return true;
+    if (typeof value === 'object') {
+        if (Array.isArray(value)) {
+            return value.every(isJsonSafe);
+        }
+        return Object.values(value as Record<string, unknown>).every(isJsonSafe);
+    }
+    throw new Error(`isJsonSafe not implemented for ${typeof value} ${value}`);
 }
