0671680a16fa3b567271946fdcb6dcb80de89dce
John Spurlock <john@skymethod.com>
2021-09-18 19:35:56 -0500

    Bump cli version after release. Migrate webtail deps to use df v0.2.0.

--- cli/cli_version.ts
+++ cli/cli_version.ts
@@ -1,1 +1,1 @@
-export const CLI_VERSION = '0.2.0';
+export const CLI_VERSION = '0.2.0+';
--- examples/webtail-worker/deps_worker.ts
+++ examples/webtail-worker/deps_worker.ts
@@ -1,2 +1,2 @@
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/f7bc536c3defc51951ea6c2c01444a10fcddab26/common/bytes.ts';
-export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/f7bc536c3defc51951ea6c2c01444a10fcddab26/common/cloudflare_workers_types.d.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.2.0/common/bytes.ts';
+export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.2.0/common/cloudflare_workers_types.d.ts';
