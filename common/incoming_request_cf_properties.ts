eb5bf2aaa797f5084e11c206355752ae4877f81c
John Spurlock <john@skymethod.com>
2021-09-19 19:25:55 -0500

    serve: Include cf.asn in the default mock request.cf.

--- common/incoming_request_cf_properties.ts
+++ common/incoming_request_cf_properties.ts
@@ -2,5 +2,5 @@ import { IncomingRequestCfProperties } from './cloudflare_workers_types.d.ts';
 
 export function makeIncomingRequestCfProperties(): IncomingRequestCfProperties {
     // deno-lint-ignore no-explicit-any
-    return { colo: 'DNO' } as any;
+    return { colo: 'DNO', asn: 13335 } as any;
 }
