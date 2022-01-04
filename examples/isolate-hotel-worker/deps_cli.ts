43bfcb7f580bebc73a9b64a1f3f0e8790d9c0079
John Spurlock <john@skymethod.com>
2022-01-04 12:18:05 -0600

    Update example deps to 0.4.0.

--- examples/isolate-hotel-worker/deps_cli.ts
+++ examples/isolate-hotel-worker/deps_cli.ts
@@ -1,4 +1,4 @@
 export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.114.0/path/mod.ts';
 export { parse as parseFlags } from 'https://deno.land/std@0.114.0/flags/mod.ts';
-export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.3.0/cli/module_watcher.ts';
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.3.0/common/bytes.ts';
+export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.0/cli/module_watcher.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.0/common/bytes.ts';
