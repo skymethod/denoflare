0e9af065fd20b285a2f5f016e0b88d9227bd429a
John Spurlock <john@skymethod.com>
2022-01-04 10:09:31 -0600

    serve: Support remote module specifiers to importWasm in localIsolation = none mode.

--- common/import_wasm.ts
+++ common/import_wasm.ts
@@ -11,6 +11,24 @@ import { resolve, fromFileUrl, toFileUrl } from 'https://deno.land/std@0.119.0/p
  *   prior to Cloudflare upload, so that wasm works properly in Cloudflare as well.
  */
 export async function importWasm(importMetaUrl: string, moduleSpecifier: string): Promise<WebAssembly.Module> {
+    if (moduleSpecifier.startsWith('https://')) {
+        // remote module specifier
+        const res = await fetch(moduleSpecifier);
+        if (res.status !== 200) throw new Error(`importWasm: Bad status ${res.status}, expected 200 for ${moduleSpecifier}`);
+        const contentType = (res.headers.get('content-type') || '').toLowerCase();
+        if (contentType === 'application/wasm') {
+            const { module } = await WebAssembly.instantiateStreaming(res);
+            return module;
+        } else if (contentType === 'application/octet-stream') {
+            // currently served by https://raw.githubusercontent.com
+            // allow it for now
+            const { module } = await WebAssembly.instantiate(await res.arrayBuffer());
+            return module;
+        } else {
+            throw new Error(`importWasm: Bad contentType ${contentType}, expected application/wasm or application/octet-stream for ${moduleSpecifier}`);
+        }
+    }
+    // local module specifier
     const { module } = await WebAssembly.instantiateStreaming(await fetch(toFileUrl(resolve(resolve(fromFileUrl(importMetaUrl), '..'), moduleSpecifier)).toString()));
     return module;
 }
