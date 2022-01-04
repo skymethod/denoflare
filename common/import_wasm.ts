24ddd213f12f73c281188dd052a00ab7bef1eb87
John Spurlock <john@skymethod.com>
2022-01-04 10:19:34 -0600

    serve: Update importWasm to support https importMetaUrls.

--- common/import_wasm.ts
+++ common/import_wasm.ts
@@ -12,23 +12,36 @@ import { resolve, fromFileUrl, toFileUrl } from 'https://deno.land/std@0.119.0/p
  */
 export async function importWasm(importMetaUrl: string, moduleSpecifier: string): Promise<WebAssembly.Module> {
     if (moduleSpecifier.startsWith('https://')) {
-        // remote module specifier
-        const res = await fetch(moduleSpecifier);
-        if (res.status !== 200) throw new Error(`importWasm: Bad status ${res.status}, expected 200 for ${moduleSpecifier}`);
-        const contentType = (res.headers.get('content-type') || '').toLowerCase();
-        if (contentType === 'application/wasm') {
-            const { module } = await WebAssembly.instantiateStreaming(res);
-            return module;
-        } else if (contentType === 'application/octet-stream') {
-            // currently served by https://raw.githubusercontent.com
-            // allow it for now
-            const { module } = await WebAssembly.instantiate(await res.arrayBuffer());
-            return module;
-        } else {
-            throw new Error(`importWasm: Bad contentType ${contentType}, expected application/wasm or application/octet-stream for ${moduleSpecifier}`);
-        }
+        return await instantiateModuleFromHttps(moduleSpecifier);
     }
-    // local module specifier
-    const { module } = await WebAssembly.instantiateStreaming(await fetch(toFileUrl(resolve(resolve(fromFileUrl(importMetaUrl), '..'), moduleSpecifier)).toString()));
-    return module;
+
+    if (importMetaUrl.startsWith('file://')) {
+        const { module } = await WebAssembly.instantiateStreaming(await fetch(toFileUrl(resolve(resolve(fromFileUrl(importMetaUrl), '..'), moduleSpecifier)).toString()));
+        return module;
+    } else if (importMetaUrl.startsWith('https://')) {
+        const { pathname, origin } = new URL(importMetaUrl);
+        const wasmUrl = origin + resolve(resolve(pathname, '..'), moduleSpecifier);
+        return await instantiateModuleFromHttps(wasmUrl);
+    } else {
+        throw new Error(`importWasm: Unsupported importMetaUrl: ${importMetaUrl}`);
+    }
+}
+
+//
+
+async function instantiateModuleFromHttps(url: string): Promise<WebAssembly.Module> {
+    const res = await fetch(url);
+    if (res.status !== 200) throw new Error(`importWasm: Bad status ${res.status}, expected 200 for ${url}`);
+    const contentType = (res.headers.get('content-type') || '').toLowerCase();
+    if (contentType === 'application/wasm') {
+        const { module } = await WebAssembly.instantiateStreaming(res);
+        return module;
+    } else if (contentType === 'application/octet-stream') {
+        // currently served by https://raw.githubusercontent.com
+        // allow it for now
+        const { module } = await WebAssembly.instantiate(await res.arrayBuffer());
+        return module;
+    } else {
+        throw new Error(`importWasm: Bad contentType ${contentType}, expected application/wasm or application/octet-stream for ${url}`);
+    }
 }
