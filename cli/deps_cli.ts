82d60c004a4f9b37069233d245bf1d766170b508
John Spurlock <john@skymethod.com>
2022-01-17 12:32:16 -0600

    serve: Update to latest deno_graph to fix panics on projects with import json assertions.

--- cli/deps_cli.ts
+++ cli/deps_cli.ts
@@ -2,8 +2,8 @@ export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, rela
 export { parse as parseFlags } from 'https://deno.land/std@0.114.0/flags/mod.ts';
 export { ensureDir, walk, emptyDir } from 'https://deno.land/std@0.114.0/fs/mod.ts';
 
-export { createGraph } from 'https://deno.land/x/deno_graph@0.11.1/mod.ts';
-export type { ModuleGraphJson } from 'https://deno.land/x/deno_graph@0.11.1/lib/types.d.ts';
+export { createGraph } from 'https://deno.land/x/deno_graph@0.18.0/mod.ts';
+export type { ModuleGraphJson } from 'https://deno.land/x/deno_graph@0.18.0/lib/types.d.ts';
 export { gzip } from 'https://deno.land/x/compress@v0.4.1/zlib/mod.ts';
 export { parse as _parseJsonc } from 'https://cdn.skypack.dev/jsonc-parser@3.0.0';
 export { default as marked } from 'https://cdn.skypack.dev/marked@3.0.2?dts';
