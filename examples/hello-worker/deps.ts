eba1aa616495f20a00a51e972bf73b442b28ff91
John Spurlock <john@skymethod.com>
2022-01-24 17:52:03 -0600

    Update denoflare version references.

--- examples/hello-worker/deps.ts
+++ examples/hello-worker/deps.ts
@@ -1,1 +1,1 @@
-export type { IncomingRequestCf } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/common/cloudflare_workers_types.d.ts';
+export type { IncomingRequestCf } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.4/common/cloudflare_workers_types.d.ts';
--- examples/webtail-worker/deps_worker.ts
+++ examples/webtail-worker/deps_worker.ts
@@ -1,2 +1,2 @@
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/common/bytes.ts';
-export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.3/common/cloudflare_workers_types.d.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.4/common/bytes.ts';
+export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.4/common/cloudflare_workers_types.d.ts';
