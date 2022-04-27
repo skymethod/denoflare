79953200d1c794b52b980a36b042b8926e222a7d
John Spurlock <john@skymethod.com>
2022-04-27 17:39:22 -0500

    chore: Update all std lib refs to 0.136.0, corresponds with current Deno v1.21.0.

--- examples/webtail-worker/deps_cli.ts
+++ examples/webtail-worker/deps_cli.ts
@@ -1,4 +1,4 @@
-export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.114.0/path/mod.ts';
-export { parse as parseFlags } from 'https://deno.land/std@0.114.0/flags/mod.ts';
+export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.136.0/path/mod.ts';
+export { parse as parseFlags } from 'https://deno.land/std@0.136.0/flags/mod.ts';
 export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.5/cli/module_watcher.ts';
 export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.4.5/common/bytes.ts';
