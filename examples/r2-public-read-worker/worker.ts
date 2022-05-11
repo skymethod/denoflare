acfc4ab376dc42b5a4fe9389a18e37bf9f8c72aa
John Spurlock <john@skymethod.com>
2022-05-11 13:00:29 -0500

    public-read: Finish range handling. Proper response headers and decline to return partial responses if content is gzipped.

--- examples/r2-public-read-worker/worker.ts
+++ examples/r2-public-read-worker/worker.ts
@@ -7,6 +7,9 @@ export default {
         try {
             return await computeResponse(request, env);
         } catch (e) {
+            if (typeof e === 'object' && e.message === 'The requested range is not satisfiable') {
+                return new Response(e.message, { status: 416 });
+            }
             return new Response(`${e.stack || e}`, { status: 500 });
         }
     }
@@ -24,50 +27,63 @@ async function computeResponse(request: IncomingRequestCf, env: WorkerEnv): Prom
     console.log(`${method} ${url}`);
     if (pushId) console.log(`pushId: ${pushId}`);
 
-    const { pathname } = new URL(request.url);
-    let key = pathname.substring(1);
-    if (method === 'GET' || method === 'HEAD') {
-        if (flags.has('disallowRobots') && key === 'robots.txt') {
-            return new Response(method === 'GET' ? 'User-agent: *\nDisallow: /' : undefined, { headers: { 'content-type': TEXT_PLAIN_UTF8 }});
-        }
-        let obj: R2Object | null = null;
-        const getOrHead: (key: string, options?: R2GetOptions) => Promise<R2Object | null> = (key, options) => method === 'GET' ? bucket.get(key, options) : bucket.head(key, options);
-        if (key !== '_headers') {
-            const range = tryParseRange(headers);
-            obj = key === '' ? null : await getOrHead(key, { range });
-            if (!obj) {
-                if (key === '' || key.endsWith('/')) {
-                    key += 'index.html';
-                    obj = await getOrHead(key, { range });
-                } else {
-                    key += '/index.html';
-                    obj = await bucket.head(key);
-                    if (obj) {
-                        return new Response(undefined, { status: 308, headers: { 'location': pathname + '/' } });
-                    }
-                }
-            }
-            if (obj) {
-                return computeObjResponse(obj, range ? 206 : 200);
-            }
-        }
-        // 404
-        obj = await getOrHead('404.html');
-        if (obj) {
-            return computeObjResponse(obj, 404);
-        }
-        return new Response(method === 'GET' ? 'not found' : undefined, { status: 404, headers: { 'content-type': TEXT_PLAIN_UTF8 } });
-    } else {
+    if (method !== 'GET' && method !== 'HEAD') {
         return new Response(`Method '${method}' not allowed`, { status: 405 });
     }
+
+    const { pathname } = new URL(request.url);
+    let key = pathname.substring(1); // strip leading slash
+
+    // special handling for robots.txt
+    if (flags.has('disallowRobots') && key === 'robots.txt') {
+        return new Response(method === 'GET' ? 'User-agent: *\nDisallow: /' : undefined, { headers: { 'content-type': TEXT_PLAIN_UTF8 }});
+    }
+
+    let obj: R2Object | null = null;
+    const getOrHead: (key: string, options?: R2GetOptions) => Promise<R2Object | null> = (key, options) => method === 'GET' ? bucket.get(key, options) : bucket.head(key, options);
+    if (key !== '_headers') { // special handling for _headers, we'll process this later
+        // first, try to request the object at the given key
+        let range = method === 'GET' ? tryParseRange(headers) : undefined;
+        obj = key === '' ? null : await getOrHead(key, { range });
+        if (!obj) {
+            if (key === '' || key.endsWith('/')) { // object not found, append index.html and try again (like pages)
+                key += 'index.html';
+                obj = await getOrHead(key, { range });
+            } else { // object not found, redirect non-trailing slash to trailing slash (like pages) if index.html exists
+                key += '/index.html';
+                obj = await bucket.head(key);
+                if (obj) {
+                    return new Response(undefined, { status: 308, headers: { 'location': pathname + '/' } });
+                }
+            }
+        }
+        if (obj) {
+            // choose not to satisfy range requests for encoded content
+            if (range && computeHeaders(obj, range).get('content-encoding') === 'gzip') {
+                // re-request without range
+                range = undefined;
+                obj = await bucket.get(key);
+                if (obj === null) throw new Error(`Object ${key} existed for .get with range, but not without`);
+            }
+            return computeObjResponse(obj, range ? 206 : 200, range);
+        }
+    }
+
+    // R2 object not found, respond with 404
+    obj = await getOrHead('404.html'); // like pages
+    if (obj) {
+        return computeObjResponse(obj, 404);
+    }
+    return new Response(method === 'GET' ? 'not found' : undefined, { status: 404, headers: { 'content-type': TEXT_PLAIN_UTF8 } });
+
 }
 
 function isR2ObjectBody(obj: R2Object): obj is R2ObjectBody {
     return 'body' in obj;
 }
 
-function computeObjResponse(obj: R2Object, status: number): Response {
-    const headers = computeHeaders(obj);
+function computeObjResponse(obj: R2Object, status: number, range?: R2Range): Response {
+    const headers = computeHeaders(obj, range);
     let body = isR2ObjectBody(obj) ? obj.body : undefined;
     if (headers.get('content-encoding') === 'gzip' && headers.get('cache-control') === 'no-transform') {
         // r2 bug: cf will double gzip in this case!
@@ -82,8 +98,13 @@ function computeObjResponse(obj: R2Object, status: number): Response {
     return new Response(body, { status, headers });
 }
 
-function computeHeaders(obj: R2Object): Headers {
+function computeHeaders(obj: R2Object, range?: R2Range): Headers {
     const headers = new Headers();
+    // obj.size represents the full size, but seems to be clamped by the cf frontend down to the actual number of bytes in the partial response
+    // exactly what we want
+    headers.set('content-length', String(obj.size));
+    if (range) headers.set('content-range', `bytes ${range.offset}-${Math.min(range.offset + range.length - 1, obj.size)}/${obj.size}`);
+
     // obj.writeHttpMetadata(headers); // r2 bug: currently returns content-encoding and cache-control in content-disposition!
     // for now, don't trust any header except content-type
     // and try to move content-dispositions that look like known content-encoding or cache-control values
@@ -108,7 +129,8 @@ function computeHeaders(obj: R2Object): Headers {
 }
 
 function tryParseRange(headers: Headers): R2Range | undefined {
-    const m = /^bytes=(\d+)-(\d+)$/.exec(headers.get('range') || ''); // cf bucket api only supports byte ranges with bounded start and end
+    // cf bucket api only supports byte ranges with bounded start and end
+    const m = /^bytes=(\d+)-(\d+)$/.exec(headers.get('range') || '');
     if (!m) return undefined;
     const offset = parseInt(m[1]);
     const length = parseInt(m[2]) - offset + 1;
