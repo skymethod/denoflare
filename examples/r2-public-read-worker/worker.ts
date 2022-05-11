ea7c20a0dca9b5cfd8f4d7f0b20dce7e8cbb5dca
John Spurlock <john@skymethod.com>
2022-05-11 11:52:07 -0500

    public-read: Range requests, common get/head routing, redirects to slash like pages.

--- examples/r2-public-read-worker/worker.ts
+++ examples/r2-public-read-worker/worker.ts
@@ -1,40 +1,13 @@
-import { IncomingRequestCf, R2Bucket, R2Object, R2ObjectBody } from './deps.ts';
+import { IncomingRequestCf, R2Object, R2ObjectBody, R2Range, R2GetOptions } from './deps.ts';
 import { WorkerEnv } from './worker_env.d.ts';
 
 export default {
 
     async fetch(request: IncomingRequestCf, env: WorkerEnv): Promise<Response> {
-        const { bucket, pushId } = env;
-        const { method, url } = request;
-        console.log(`${method} ${url} pushId=${pushId}`);
-    
-        const { pathname } = new URL(request.url);
-
-        let key = pathname.substring(1);
-        if (key === '') key = 'index.html';
-
-        if (method === 'GET') {
-            if (key === 'robots.txt') {
-                return new Response('User-agent: *\nDisallow: /');
-            }
-            const obj = await bucket.get(key);
-            if (obj) {
-                return computeGetResponse(obj, 200);
-            }
-            key = key + '/index.html';
-            const obj2 = await bucket.get(key);
-            if (obj2) {
-                return computeGetResponse(obj2, 200);
-            }
-            return await computeNotFound(bucket, 'GET');
-        } else if (method === 'HEAD') {
-            const obj = await bucket.head(key);
-            if (obj) {
-                return new Response(undefined, { headers: computeHeaders(obj) });
-            }
-            return await computeNotFound(bucket, 'HEAD');
-        } else {
-            return new Response(`Method '${method}' not allowed`, { status: 405 });
+        try {
+            return await computeResponse(request, env);
+        } catch (e) {
+            return new Response(`${e.stack || e}`, { status: 500 });
         }
     }
 
@@ -42,34 +15,71 @@ export default {
 
 //
 
-function computeGetResponse(obj: R2ObjectBody, status: number): Response {
+const TEXT_PLAIN_UTF8 = 'text/plain; charset=utf-8';
+
+async function computeResponse(request: IncomingRequestCf, env: WorkerEnv): Promise<Response> {
+    const { bucket, pushId } = env;
+    const flags = new Set((env.flags || '').split(',').map(v => v.trim()));
+    const { method, url, headers } = request;
+    console.log(`${method} ${url}`);
+    if (pushId) console.log(`pushId: ${pushId}`);
+
+    const { pathname } = new URL(request.url);
+    let key = pathname.substring(1);
+    if (method === 'GET' || method === 'HEAD') {
+        if (flags.has('disallowRobots') && key === 'robots.txt') {
+            return new Response(method === 'GET' ? 'User-agent: *\nDisallow: /' : undefined, { headers: { 'content-type': TEXT_PLAIN_UTF8 }});
+        }
+        let obj: R2Object | null = null;
+        const getOrHead: (key: string, options?: R2GetOptions) => Promise<R2Object | null> = (key, options) => method === 'GET' ? bucket.get(key, options) : bucket.head(key, options);
+        if (key !== '_headers') {
+            const range = tryParseRange(headers);
+            obj = key === '' ? null : await getOrHead(key, { range });
+            if (!obj) {
+                if (key === '' || key.endsWith('/')) {
+                    key += 'index.html';
+                    obj = await getOrHead(key, { range });
+                } else {
+                    key += '/index.html';
+                    obj = await bucket.head(key);
+                    if (obj) {
+                        return new Response(undefined, { status: 308, headers: { 'location': pathname + '/' } });
+                    }
+                }
+            }
+            if (obj) {
+                return computeObjResponse(obj, range ? 206 : 200);
+            }
+        }
+        // 404
+        obj = await getOrHead('404.html');
+        if (obj) {
+            return computeObjResponse(obj, 404);
+        }
+        return new Response(method === 'GET' ? 'not found' : undefined, { status: 404, headers: { 'content-type': TEXT_PLAIN_UTF8 } });
+    } else {
+        return new Response(`Method '${method}' not allowed`, { status: 405 });
+    }
+}
+
+function isR2ObjectBody(obj: R2Object): obj is R2ObjectBody {
+    return 'body' in obj;
+}
+
+function computeObjResponse(obj: R2Object, status: number): Response {
     const headers = computeHeaders(obj);
-    let body = obj.body;
+    let body = isR2ObjectBody(obj) ? obj.body : undefined;
     if (headers.get('content-encoding') === 'gzip' && headers.get('cache-control') === 'no-transform') {
         // r2 bug: cf will double gzip in this case!
         // for now, decompress it in the worker and let cf autocompress take over
-        const ds = new DecompressionStream('gzip');
-        body = obj.body.pipeThrough(ds);
+        if (body) {
+            const ds = new DecompressionStream('gzip');
+            body = body.pipeThrough(ds);
+        }
         headers.delete('content-encoding');
         headers.delete('cache-control');
     }
     return new Response(body, { status, headers });
-}
-
-async function computeNotFound(bucket: R2Bucket, method: 'GET' | 'HEAD'): Promise<Response> {
-    const key = '404.html';
-    if (method === 'GET') {
-        const obj = await bucket.get(key);
-        if (obj) {
-            return computeGetResponse(obj, 404)
-        }
-    } else if (method === 'HEAD') {
-        const obj = await bucket.head(key);
-        if (obj) {
-            return new Response(undefined, { status: 404, headers: computeHeaders(obj) });
-        }
-    }
-    return new Response(method === 'GET' ? 'not found' : undefined, { status: 404, headers: { 'content-type': 'text/plain; charset=utf-8' } });
 }
 
 function computeHeaders(obj: R2Object): Headers {
@@ -95,4 +105,13 @@ function computeHeaders(obj: R2Object): Headers {
     if (cacheControl) headers.set('x-r2-cache-control', cacheControl);
     if (cacheExpiry) headers.set('x-r2-cache-expiry', cacheExpiry.toISOString());
     return headers;
+}
+
+function tryParseRange(headers: Headers): R2Range | undefined {
+    const m = /^bytes=(\d+)-(\d+)$/.exec(headers.get('range') || ''); // cf bucket api only supports byte ranges with bounded start and end
+    if (!m) return undefined;
+    const offset = parseInt(m[1]);
+    const length = parseInt(m[2]) - offset + 1;
+    if (length < 1) return undefined;
+    return { offset, length };
 }
--- examples/r2-public-read-worker/worker_env.d.ts
+++ examples/r2-public-read-worker/worker_env.d.ts
@@ -3,4 +3,5 @@ import { R2Bucket } from './deps.ts';
 export interface WorkerEnv {
     readonly bucket: R2Bucket;
     readonly pushId?: string;
+    readonly flags?: string;
 }
