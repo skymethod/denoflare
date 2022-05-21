a23fb2ba6759276708c8229eb8afa3730574fce8
John Spurlock <john@skymethod.com>
2022-05-21 18:40:52 -0500

    examples: Fix presigned-url deps.

--- examples/r2-presigned-url-worker/deps.ts
+++ examples/r2-presigned-url-worker/deps.ts
@@ -1,3 +1,4 @@
 export type { IncomingRequestCf, R2Bucket, R2Object, R2ObjectBody, R2Range, R2GetOptions, R2Conditional, R2Objects, R2ListOptions } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/cloudflare_workers_types.d.ts';
 export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/bytes.ts';
-export { AwsCall, computeExpectedAwsSignature, R2, tryParseAmazonDate } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/r2/r2.ts';
+export { computeExpectedAwsSignature, tryParseAmazonDate, R2 } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/r2/r2.ts';
+export type { AwsCall } from 'https://raw.githubusercontent.com/skymethod/denoflare/100b2e4cc824860daff2ad242e3ef404571c0a35/common/r2/r2.ts';
