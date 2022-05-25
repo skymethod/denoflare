874d14a54eacadf0e23ba0be7570162297d382c3
John Spurlock <john@skymethod.com>
2022-05-25 14:17:55 -0500

    Update versions prior to release.

--- examples/hello-wasm-worker/deps.ts
+++ examples/hello-wasm-worker/deps.ts
@@ -1,1 +1,1 @@
-export { importWasm } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.0/common/import_wasm.ts';
+export { importWasm } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/import_wasm.ts';
--- examples/webtail-worker/deps_worker.ts
+++ examples/webtail-worker/deps_worker.ts
@@ -1,3 +1,3 @@
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.0/common/bytes.ts';
-export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.0/common/cloudflare_workers_types.d.ts';
-export { encodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.0/common/xml_util.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/bytes.ts';
+export type { IncomingRequestCf, ModuleWorkerContext } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/cloudflare_workers_types.d.ts';
+export { encodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/xml_util.ts';
