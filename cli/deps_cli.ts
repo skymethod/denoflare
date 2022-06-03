cc3c46d3363d62549f7b856ad8a07610023bde81
John Spurlock <john@skymethod.com>
2022-06-03 16:39:36 -0500

    Update to latest std@ version.

--- cli/deps_cli.ts
+++ cli/deps_cli.ts
@@ -1,6 +1,6 @@
-export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.140.0/path/mod.ts';
-export { parse as parseFlags } from 'https://deno.land/std@0.140.0/flags/mod.ts';
-export { ensureDir, walk, emptyDir } from 'https://deno.land/std@0.140.0/fs/mod.ts';
+export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.142.0/path/mod.ts';
+export { parse as parseFlags } from 'https://deno.land/std@0.142.0/flags/mod.ts';
+export { ensureDir, walk, emptyDir } from 'https://deno.land/std@0.142.0/fs/mod.ts';
 
 export { createGraph } from 'https://deno.land/x/deno_graph@0.18.0/mod.ts';
 export type { ModuleGraphJson } from 'https://deno.land/x/deno_graph@0.18.0/lib/types.d.ts';
@@ -9,6 +9,6 @@ export { parse as _parseJsonc } from 'https://cdn.skypack.dev/jsonc-parser@3.0.0
 export { default as marked } from 'https://cdn.skypack.dev/marked@3.0.2?dts';
 export { html } from 'https://deno.land/x/html_escape@v1.1.5/html.ts';
 export { default as hljs } from 'https://cdn.skypack.dev/highlight.js@11.2.0';
-export { Md5 } from 'https://deno.land/std@0.140.0/hash/md5.ts';
-export { crypto as WasmCrypto } from 'https://deno.land/std@0.140.0/crypto/mod.ts';
-export { serve } from 'https://deno.land/std@0.140.0/http/server.ts';
+export { Md5 } from 'https://deno.land/std@0.142.0/hash/md5.ts';
+export { crypto as WasmCrypto } from 'https://deno.land/std@0.142.0/crypto/mod.ts';
+export { serve } from 'https://deno.land/std@0.142.0/http/server.ts';
--- common/xml_util_test.ts
+++ common/xml_util_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.140.0/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.142.0/testing/asserts.ts';
 
 import { decodeXml, encodeXml } from './xml_util.ts';
 
