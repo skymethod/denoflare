8dcb48c1c498c7e3cd1665e1cd1e9e8efdec4af6
John Spurlock <john@skymethod.com>
2022-06-16 13:08:53 -0500

    dev: Remove the old npm generator.

--- devcli/devcli.ts
+++ devcli/devcli.ts
@@ -2,7 +2,6 @@ import { CliCommand } from '../cli/cli_command.ts';
 import { parseFlags } from '../cli/flag_parser.ts';
 import { auth, AUTH_COMMAND } from './devcli_auth.ts';
 import { generateNpm } from './devcli_generate_npm.ts';
-import { generateNpmNew } from './devcli_generate_npm_new.ts';
 import { generateReasonCodes } from './devcli_pubsub.ts';
 import { tmp as r2Tmp } from './devcli_r2.ts';
 import { regenerateDocs, REGENERATE_DOCS_COMMAND } from './devcli_regenerate_docs.ts';
@@ -15,5 +14,5 @@ export const DENOFLAREDEV_COMMAND = CliCommand.of(['denoflaredev'])
     ;
 
 if (import.meta.main) {
-    await DENOFLAREDEV_COMMAND.routeSubcommand(args, options, { generateNpm, generateNpmNew, generateReasonCodes, r2Tmp });
+    await DENOFLAREDEV_COMMAND.routeSubcommand(args, options, { generateNpm, generateReasonCodes, r2Tmp });
 }
--- devcli/devcli_generate_npm.ts
+++ devcli/devcli_generate_npm.ts
@@ -1,72 +1,120 @@
-// deno-lint-ignore-file no-explicit-any
-import { fromFileUrl, resolve, basename } from './deps.ts';
+import { bundle } from '../cli/bundle.ts';
+import { fromFileUrl, resolve, basename, walk } from './deps.ts';
+import { transform, stop } from 'https://deno.land/x/esbuild@v0.14.43/mod.js';
+import { parseOptionalBooleanOption } from "../cli/cli_common.ts";
 
-// Requires Deno 1.21.3 or below (RIP Deno.emit)
-
-export async function generateNpm(_args: (string | number)[], _options: Record<string, unknown>) {
-    await generateEsmMainJs();
-    await generateCjsMainJs();
-    await generateMainTypes();
+export async function generateNpm(_args: (string | number)[], options: Record<string, unknown>) {
+    const verbose = parseOptionalBooleanOption('verbose', options);
+    try {
+        await generateEsmMainJs();
+        await generateCjsMainJs();
+        await generateMainTypes({ verbose });
+    } finally {
+        stop(); // otherwise esbuild hangs
+    }
 }
 
 //
 
 async function generateEsmMainJs() {
-    const contents = await generateBundleContents({ target: 'es2019' }); // remove optional chaining for esm too, to support folks using modules, but still on older environments
+    const contents = await generateBundleContents({ format: 'esm', target: 'es2019' }); // remove optional chaining for esm too, to support folks using modules, but still on older environments
     await saveContentsIfChanged('../../npm/denoflare-mqtt/esm/main.js', contents);
 }
 
 async function generateCjsMainJs() {
-    const contents = (await generateBundleContents({ target: 'es2019' })) // remove optional chaining
+    const contents = (await generateBundleContents({ format: 'cjs', target: 'es2019' })) // remove optional chaining
         .replaceAll(/export { ([A-Z0-9a-z_]+) as ([A-Z0-9a-z_]+) };/g, 'exports.$2 = $1;');
     await saveContentsIfChanged('../../npm/denoflare-mqtt/cjs/main.js', contents);
 }
 
-async function generateMainTypes() {
-    // requires --unstable
-    const result = await (Deno as any).emit(resolveRelativeFile('../../common/mqtt/mqtt_client.ts'), { 
-        // bundle: 'classic', 
-        compilerOptions: { 
-            declaration: true, // doesn't work with bundle: module
-            emitDeclarationOnly: true,
-            removeComments: false,
-        },
-    });
-    const declaration1 = Object.entries(result.files).filter(v => v[0].endsWith('/mqtt_client.ts.d.ts'))[0][1] as string;
-    const contents1 = declaration1
-        .replaceAll(/\/\/\/ <amd-module name=".*?" \/>\s+/g, '')
-        .replaceAll(/\n\s+private .*?;/g, '')
-        .replaceAll(/import .*?;\n/g, '')
-        ;
-    
-    const declaration2 = Object.entries(result.files).filter(v => v[0].endsWith('/mqtt_messages.ts.d.ts'))[0][1] as string;
-    const contents2 = declaration2
-        .replaceAll(/\/\/\/ <amd-module name=".*?" \/>\s+/g, '')
-        .replaceAll(/\n\/\*\*\s+@internal1.*?;.*?;/gs, '')
-        .replaceAll(/\n\/\*\*\s+@internalclass.*?\n}/gs, '')
-        .replaceAll(/\n\/\*\*\s+@internal.*?;/gs, '')
-        ;
+async function generateMainTypes(opts: { verbose?: boolean } = {}) {
+    const { verbose = false } = opts;
 
-    await saveContentsIfChanged('../../npm/denoflare-mqtt/main.d.ts', [ contents1, contents2].join('\n\n\n'));
+    // nothing but failure so far
+    // https://github.com/denoland/deno_emit/issues/29#issuecomment-1157902613
+    
+    // use tsc in the meantime
+
+    const files = [ resolveRelativeFile('../../common/mqtt/mqtt_client.ts'), resolveRelativeFile('../../common/mqtt/mqtt_messages.ts') ];
+    const compilerOptions = {
+        declaration: true,
+        emitDeclarationOnly: true,
+        removeComments: false,
+        stripInternal: true,
+        lib: [ 'esnext' ],
+    };
+    const result = await runTsc({ files, compilerOptions });
+    if (verbose) console.log(result);
+    if (verbose) console.log(result.out);
+
+    const client = result.output['mqtt_client.d.ts'].replaceAll(/import .*?;\n/g, '');
+    const messages = result.output['mqtt_messages.d.ts'];
+
+    await saveContentsIfChanged('../../npm/denoflare-mqtt/main.d.ts', [ client, messages].join('\n\n\n')); 
+
+    // const tsconfigRaw = JSON.stringify({ compilerOptions2: { emitDeclarationOnly: true, removeComments: false } });
+    // console.log(tsconfigRaw);
+
+    // const output = await transform(await Deno.readTextFile(resolveRelativeFile('../../common/mqtt/mqtt_client.ts')), { tsconfigRaw: {  compilerOptions: { }}, loader: 'ts' });
+    // console.log(output.code);
+    // const tsconfig = await Deno.makeTempFile({ prefix: 'generate-npm-esbuild-tsconfig', suffix: '.json'});
+    // await Deno.writeTextFile(tsconfig, JSON.stringify({ compilerOptions2: { emitDeclarationOnly: true, removeComments: false } }));
+    // try {
+    //     const output = await build({ loader: '', tsconfig: tsconfig, entryPoints: [ resolveRelativeFile('../../common/mqtt/mod_iso.ts')] });
+    //     console.log(output);
+    // } finally {
+    //     await Deno.remove(tsconfig);
+    // }
 }
 
-async function generateBundleContents(opts: { target?: string }): Promise<string> {
-    const { target } = opts;
+async function generateBundleContents(opts: { format: 'cjs' | 'esm', target?: string }): Promise<string> {
+    const { format, target } = opts;
 
-    // requires --unstable
-    const result = await (Deno as any).emit(resolveRelativeFile('../../common/mqtt/mqtt_client.ts'), { 
-        bundle: 'module'
-    });
-    const js = result.files['deno:///bundle.js'];
-    if (!target) return js;
+    const { code } = await bundle(resolveRelativeFile('../../common/mqtt/mod_iso.ts')); // deno bundle does not support 'target' as a compilerOption
 
-    // transpilation
-    // https://github.com/denoland/deno/issues/9638#issuecomment-982748670
-    const transpiled = await (Deno as any).emit('/src.ts', {
-        sources: { '/src.ts': js },
-        compilerOptions: { target },
-    });
-    return transpiled.files['file:///src.ts.js'];
+    // try to use wasm instead of bundled esbuild binary
+    // await initialize({ wasmURL: 'https://unpkg.com/esbuild-wasm@0.14.43/esbuild.wasm' }); // Error: The "wasmURL" option only works in the browser
+
+    // this doesn't work either
+    // https://github.com/evanw/esbuild/issues/2323
+    // const wasmModule = await importWasm(import.meta.url, 'https://unpkg.com/esbuild-wasm@0.14.43/esbuild.wasm');
+    // console.log({ wasmModule });
+    // await initialize({ wasmModule, worker: false }); 
+
+    const { code: code2 } = await transform(code, {  format,  target });
+    return code2;
+}
+
+type TscResult = { status: { success: boolean, code: number }, out: string, err: string, output: Record<string, string> };
+
+async function runTsc(opts: { files: string[], compilerOptions: Record<string, unknown> }): Promise<TscResult> {
+    const { files, compilerOptions } = opts;
+    const tsconfigFile = await Deno.makeTempFile({ prefix: 'run-tsc-tsconfig', suffix: '.json'});
+    const outDir = await Deno.makeTempDir({ prefix: 'run-tsc-outdir', suffix: '.json'});
+    compilerOptions.outDir = outDir;
+    try {
+        await Deno.writeTextFile(tsconfigFile, JSON.stringify({ files, compilerOptions }, undefined, 2));
+        const { status, stdout, stderr } = await Deno.spawn('/usr/local/bin/tsc', {
+            args: [
+                '--project', tsconfigFile,
+            ],
+            env: {
+                NO_COLOR: '1', // to make parsing the output easier
+            }
+        });
+        const out = new TextDecoder().decode(stdout);
+        const err = new TextDecoder().decode(stderr);
+        const output: Record<string, string> = {};
+        for await (const entry of walk(outDir, { maxDepth: 1 })) {
+            if (!entry.isFile) continue;
+            const { name, path } = entry;
+            output[name] = await Deno.readTextFile(path);
+        }
+        return { status, out, err, output };
+    } finally {
+        await Deno.remove(tsconfigFile);
+        await Deno.remove(outDir, { recursive: true });
+    }
 }
 
 function resolveRelativeFile(relativePath: string): string {
