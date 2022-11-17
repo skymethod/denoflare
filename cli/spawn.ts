3838440103a68130909d69731802a5557afec78a
John Spurlock <john@skymethod.com>
2022-11-17 11:04:17 -0600

    deno: Update spawn to use new Deno.Command on 1.28+.

--- cli/spawn.ts
+++ cli/spawn.ts
@@ -1,3 +1,5 @@
+// deno-lint-ignore-file no-explicit-any
+
 // 1.24.0 version
 export interface SpawnOutput  {
     get stdout(): Uint8Array;
@@ -7,22 +9,32 @@ export interface SpawnOutput  {
     signal: Deno.Signal | null;
 }
 
-export async function spawn(command: string | URL, options?: Deno.SpawnOptions): Promise<SpawnOutput> {
-    // deno-lint-ignore no-explicit-any
-    const rt = await Deno.spawn(command, options) as any;
-
-    // pre-1.24.0, success, code, and signal were under a .status property
-    // https://deno.com/blog/v1.24#updates-to-new-subprocess-api
-    if (typeof rt.status === 'object') {
-        const { success, code, signal } = rt.status;
-        if (typeof success === 'boolean' && typeof code === 'number' && signal !== undefined) {
-            rt.success = success;
-            rt.code = code;
-            rt.signal = signal;
-            delete rt.status;
-        } else {
-            throw new Error(`Unexpected pre-1.24.0 spawn output status object`);
-        }
+export async function spawn(command: string | URL, options?: { args?: string[], env?: Record<string, string> }): Promise<SpawnOutput> {
+    // use Deno.Command if 1.28+, spawn is deprecated
+    if ('Command' in Deno) {
+        const c = new (Deno as any).Command(command, options);
+        return await c.output();
     }
-    return rt;
+
+    // use Deno.spawn if < 1.28, not deprecated
+    if ('spawn' in Deno) {
+        const rt = await (Deno as any).spawn(command, options) as any;
+
+        // pre-1.24.0, success, code, and signal were under a .status property
+        // https://deno.com/blog/v1.24#updates-to-new-subprocess-api
+        if (typeof rt.status === 'object') {
+            const { success, code, signal } = rt.status;
+            if (typeof success === 'boolean' && typeof code === 'number' && signal !== undefined) {
+                rt.success = success;
+                rt.code = code;
+                rt.signal = signal;
+                delete rt.status;
+            } else {
+                throw new Error(`Unexpected pre-1.24.0 spawn output status object`);
+            }
+        }
+        return rt;
+    }
+
+    throw new Error(`Unable to spawn subprocess: did not find 'Deno.Command' or 'Deno.spawn'`);
 }
