5caf8e1f3c220931548d2bd88b327c30f0fd0b14
John Spurlock <john@skymethod.com>
2022-11-17 11:37:19 -0600

    config: Add serviceEnvironment to json schema and validation.

--- common/config_validation.ts
+++ common/config_validation.ts
@@ -110,14 +110,15 @@ function checkScript(name: string, script: any): Script {
 // deno-lint-ignore no-explicit-any
 function checkBinding(name: string, binding: any) {
     checkObject(name, binding);
-    const { value, secret, kvNamespace, doNamespace, wasmModule, bucketName, dataset, d1DatabaseUuid, queueName } = binding;
-    const definedCount = [ value, secret, kvNamespace, doNamespace, wasmModule, bucketName, dataset, d1DatabaseUuid, queueName ].filter(v => v !== undefined).length;
+    const { value, secret, kvNamespace, doNamespace, wasmModule, serviceEnvironment, bucketName, dataset, d1DatabaseUuid, queueName } = binding;
+    const definedCount = [ value, secret, kvNamespace, doNamespace, wasmModule, serviceEnvironment, bucketName, dataset, d1DatabaseUuid, queueName ].filter(v => v !== undefined).length;
     if (definedCount === 1) {
         if (value !== undefined && typeof value !== 'string') throw new Error(`Bad ${name}.value: expected string, found ${typeof value}`);
         else if (secret !== undefined && typeof secret !== 'string') throw new Error(`Bad ${name}.secret: expected string, found ${typeof secret}`);
         else if (kvNamespace !== undefined && typeof kvNamespace !== 'string') throw new Error(`Bad ${name}.kvNamespace: expected string, found ${typeof kvNamespace}`);
         else if (doNamespace !== undefined && typeof doNamespace !== 'string') throw new Error(`Bad ${name}.doNamespace: expected string, found ${typeof doNamespace}`);
         else if (wasmModule !== undefined && typeof wasmModule !== 'string') throw new Error(`Bad ${name}.wasmModule: expected string, found ${typeof wasmModule}`);
+        else if (serviceEnvironment !== undefined && typeof serviceEnvironment !== 'string') throw new Error(`Bad ${name}.serviceEnvironment: expected string, found ${typeof serviceEnvironment}`);
         else if (bucketName !== undefined && typeof bucketName !== 'string') throw new Error(`Bad ${name}.bucketName: expected string, found ${typeof bucketName}`);
         else if (dataset !== undefined && typeof dataset !== 'string') throw new Error(`Bad ${name}.dataset: expected string, found ${typeof dataset}`);
         else if (d1DatabaseUuid !== undefined && typeof d1DatabaseUuid !== 'string') throw new Error(`Bad ${name}.d1DatabaseUuid: expected string, found ${typeof d1DatabaseUuid}`);
