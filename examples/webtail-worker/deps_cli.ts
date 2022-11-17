b3ac5e110b08c282239730a131196aca11571797
John Spurlock <john@skymethod.com>
2022-11-17 10:48:00 -0600

    deno: Update to latest std@0.165.0.

--- examples/webtail-worker/deps_cli.ts
+++ examples/webtail-worker/deps_cli.ts
@@ -1,4 +1,4 @@
-export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.162.0/path/mod.ts';
+export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.165.0/path/mod.ts';
 export { ModuleWatcher } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.10/cli/module_watcher.ts';
 export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.10/common/bytes.ts';
 export { bundle } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.10/cli/bundle.ts';
