f7752172bfedb9d59f447c7a3b05950325aa1ee1
John Spurlock <john@skymethod.com>
2023-01-06 10:14:47 -0600

    deno: Update to std@0.171.0.

--- cli/versions_test.ts
+++ cli/versions_test.ts
@@ -1,4 +1,4 @@
-import { assert } from 'https://deno.land/std@0.168.0/testing/asserts.ts';
+import { assert } from 'https://deno.land/std@0.171.0/testing/asserts.ts';
 
 import { versionCompare } from './versions.ts';
 
