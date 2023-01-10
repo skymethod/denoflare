570d2a40fd0148568873df2f1a465805d4df8e7d
arisris <aris@mail.kliksob.com>
2023-01-10 15:38:56 +0700

    Update deno_graph version from 0.18.0 to 0.41.0

--- cli/deps_cli.ts
+++ cli/deps_cli.ts
@@ -1,8 +1,8 @@
 export { basename, dirname, join, fromFileUrl, resolve, toFileUrl, extname, relative, isAbsolute, normalize, parse as parsePath, globToRegExp } from 'https://deno.land/std@0.171.0/path/mod.ts';
 export { ensureDir, walk, emptyDir } from 'https://deno.land/std@0.171.0/fs/mod.ts';
 export { SEP as systemSeparator } from 'https://deno.land/std@0.171.0/path/separator.ts';
-export { createGraph } from 'https://deno.land/x/deno_graph@0.18.0/mod.ts';
-export type { ModuleGraphJson } from 'https://deno.land/x/deno_graph@0.18.0/lib/types.d.ts';
+export { createGraph } from 'https://deno.land/x/deno_graph@0.41.0/mod.ts';
+export type { ModuleGraphJson } from 'https://deno.land/x/deno_graph@0.41.0/lib/types.d.ts';
 export { gzip } from 'https://deno.land/x/compress@v0.4.1/zlib/mod.ts';
 export { parse as _parseJsonc } from 'https://cdn.skypack.dev/jsonc-parser@3.0.0';
 export { default as marked } from 'https://cdn.skypack.dev/marked@3.0.2?dts';
