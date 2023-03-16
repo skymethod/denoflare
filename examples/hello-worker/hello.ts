af60becd39248117b22db5eba16c106f7d91c457
John Spurlock <john@skymethod.com>
2023-03-16 12:02:23 -0500

    Examples: back to city in hello world.

--- examples/hello-worker/hello.ts
+++ examples/hello-worker/hello.ts
@@ -1,10 +1,10 @@
 import { IncomingRequestCf } from './deps.ts';
 
-// simplest possible module worker, echo back the Cloudflare colo code serving the request
+// simplest possible module worker, echo back the client's city, as determined by Cloudflare
 export default {
 
     fetch(request: IncomingRequestCf): Response {
-        const html = `<h3>Hello from ${request.cf.colo}!</h3>`;
+        const html = `<h3>Hello ${'city' in request.cf ? request.cf.city : 'nowhere'}!</h3>`;
         return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
     }
 
