1455f8510cf2485f957ba61bf36e43f7d675dc6a
John Spurlock <john@skymethod.com>
2023-03-16 11:52:02 -0500

    examples: Update hello world to use cf.colo.

--- examples/hello-worker/hello.ts
+++ examples/hello-worker/hello.ts
@@ -1,11 +1,10 @@
 import { IncomingRequestCf } from './deps.ts';
 
-// simplest possible module worker, echo back the client's city, as determined by Cloudflare
+// simplest possible module worker, echo back the Cloudflare colo code serving the request
 export default {
 
     fetch(request: IncomingRequestCf): Response {
-       // FIXME: .cf.city is not available
-        const html = `<h3>Hello ${request.cf.city}!</h3>`;
+        const html = `<h3>Hello from ${request.cf.colo}!</h3>`;
         return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' } });
     }
 
