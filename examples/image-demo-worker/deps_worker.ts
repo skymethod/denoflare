96b165b1c2bef33db036555189c8ac07f61cac6b
Nanai Jua <me@n7i.dev>
2023-03-16 11:23:34 +0900

    deno: Update deps in `**/deps_*.ts`

--- examples/image-demo-worker/deps_worker.ts
+++ examples/image-demo-worker/deps_worker.ts
@@ -1,5 +1,5 @@
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/bytes.ts';
-export type { IncomingRequestCf } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/cloudflare_workers_types.d.ts';
-export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/import_text.ts';
-export { importBinary } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/import_binary.ts';
-export { importWasm } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.1/common/import_wasm.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/bytes.ts';
+export type { IncomingRequestCf } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/cloudflare_workers_types.d.ts';
+export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/import_text.ts';
+export { importBinary } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/import_binary.ts';
+export { importWasm } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/import_wasm.ts';
--- examples/webtail-app/deps_app.ts
+++ examples/webtail-app/deps_app.ts
@@ -1,4 +1,4 @@
-export { css, html, LitElement, svg, SVGTemplateResult, CSSResult, TemplateResult } from 'https://cdn.skypack.dev/lit-element@2.5.1';
+export { css, html, LitElement, svg, SVGTemplateResult, CSSResult, TemplateResult } from 'https://cdn.skypack.dev/lit-element@3.2.2';
 export type { Tail } from '../../common/cloudflare_api.ts';
 export { createTail, CloudflareApiError, listScripts, listTails, CloudflareApi } from '../../common/cloudflare_api.ts';
 export { setSubtract, setEqual, setIntersect, setUnion } from '../../common/sets.ts';
