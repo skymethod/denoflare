afeaf442ecc1db3e266a74e7e4bce7c88d39590d
John Spurlock <john@skymethod.com>
2023-06-11 18:37:25 -0500

    Update version before release.

--- examples/mqtt-demo-worker/deps.ts
+++ examples/mqtt-demo-worker/deps.ts
@@ -1,1 +1,1 @@
-export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.11/common/import_text.ts';
+export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.12/common/import_text.ts';
