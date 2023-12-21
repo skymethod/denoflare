ab8e3488f9b07b65a9483e1c7cf4d8bbe1f3bb99
John Spurlock <john@skymethod.com>
2023-12-21 17:28:52 -0600

    Update to std@0.210.0.

--- common/analytics/r2_costs_test.ts
+++ common/analytics/r2_costs_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.208.0/assert/assert_equals.ts';
+import { assertEquals } from 'https://deno.land/std@0.210.0/assert/assert_equals.ts';
 import { computeCosts } from './r2_costs.ts';
 
 const DEBUG = false;
--- common/xml_util_test.ts
+++ common/xml_util_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.208.0/assert/assert_equals.ts';
+import { assertEquals } from 'https://deno.land/std@0.210.0/assert/assert_equals.ts';
 
 import { decodeXml, encodeXml } from './xml_util.ts';
 
