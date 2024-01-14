22311acb32ff630c1a2ea56d7028f04bdeff5a20
John Spurlock <john@skymethod.com>
2024-01-14 15:51:13 -0600

    supabase: Functions api and eszip helper.

---
+++ common/supabase/eszip.ts
@@ -0,0 +1,42 @@
+import { build, Parser } from 'https://deno.land/x/eszip@v0.57.0/mod.ts';
+import { compress } from 'https://deno.land/x/brotli@0.1.7/mod.ts';
+import { concat } from 'https://deno.land/std@0.212.0/bytes/concat.ts';
+
+export async function buildEszip(entry: string, contentFn: (spec: string) => Promise<string | undefined> | string | undefined): Promise<Uint8Array> {
+    const roots = [ entry ];
+    return await build(roots, async specifier => {
+        const content = await contentFn(specifier);
+        if (content) {
+            return {
+                kind: 'module',
+                content,
+                specifier,
+            };
+        }
+        return undefined;
+    });
+}
+
+export type EszipEntry = { specifier: string, source: string, sourceMap?: string };
+
+export async function parseEszip(bytes: Uint8Array): Promise<readonly EszipEntry[]> {
+    const parser = await Parser.createInstance();
+    const rt: EszipEntry[] = [];
+    try {
+        const specifiers = await parser.parseBytes(bytes) as string[];
+        await parser.load();
+        for (const specifier of specifiers) {
+            const source = await parser.getModuleSource(specifier) as string;
+            const sourceMap = await parser.getModuleSourceMap(specifier) as string | undefined;
+            rt.push({ specifier, source, sourceMap });
+        }
+    } finally {
+        parser.free();
+    }
+    return rt;
+}
+
+export function brotliCompressEszip(uncompressedEszip: Uint8Array): Uint8Array {
+    const compressed = compress(uncompressedEszip);
+    return concat([ new TextEncoder().encode('EZBR'), compressed ]);
+}
---
+++ common/supabase/supabase_api.ts
@@ -0,0 +1,126 @@
+
+// List all functions
+// https://supabase.com/docs/reference/api/list-all-functions
+
+export async function listFunctions({ projectRef, token, fetcher }: { projectRef: string, token: string, fetcher?: Fetcher }): Promise<ApiResponse<readonly FunctionInfo[]>> {
+    return await execute(`/v1/projects/${projectRef}/functions`, { token, fetcher }, async res => checkArrayOf(await res.json(), checkFunctionInfo));
+}
+
+export type FunctionInfo = { 
+    readonly verify_jwt: boolean, 
+    readonly id: string, // e.g. dfb28f2a-a1ae-4367-bfac-d6a333b7b13d
+    readonly slug: string, // e.g. hello-1
+    readonly name: string, // e.g. hello 1
+    readonly version: number, // e.g. 1
+    readonly status: string, // e.g. ACTIVE
+    readonly created_at: number, // e.g. 1705256686007
+    readonly updated_at: number, // e.g. 1705256686007
+    readonly import_map: boolean,
+    readonly entrypoint_path: string, // e.g. file:///src/index.ts
+};
+
+function isFunctionInfo(obj: unknown): obj is FunctionInfo {
+    return isStringRecord(obj)
+        && typeof obj.verify_jwt === 'boolean'
+        && typeof obj.id === 'string'
+        && typeof obj.slug === 'string'
+        && typeof obj.name === 'string'
+        && typeof obj.version === 'number'
+        && typeof obj.status === 'string'
+        && typeof obj.created_at === 'number'
+        && typeof obj.updated_at === 'number'
+        && typeof obj.import_map === 'boolean'
+        && typeof obj.entrypoint_path === 'string'
+        ;
+}
+
+function checkFunctionInfo(obj: unknown): FunctionInfo {
+    if (!isFunctionInfo(obj)) throw new Error(JSON.stringify(obj));
+    // deno-lint-ignore no-unused-vars
+    const { verify_jwt, id, slug, name, version, status, created_at, updated_at, import_map, entrypoint_path, ...rest } = obj;
+    if (Object.keys(rest).length > 0) throw new Error(JSON.stringify(obj));
+    return obj;
+}
+
+// Get a function
+// https://supabase.com/docs/reference/api/retrieve-a-function
+
+export async function getFunction({ projectRef, slug, token, fetcher }: { projectRef: string, slug: string, token: string, fetcher?: Fetcher }): Promise<ApiResponse<FunctionInfo>> {
+    return await execute(`/v1/projects/${projectRef}/functions/${slug}`, { token, fetcher }, async res => checkFunctionInfo(await res.json()));
+}
+
+// Get a function body
+// https://supabase.com/docs/reference/api/retrieve-a-function-body
+
+export async function getFunctionBody({ projectRef, slug, token, fetcher }: { projectRef: string, slug: string, token: string, fetcher?: Fetcher }): Promise<ApiResponse<Uint8Array>> {
+    return await execute(`/v1/projects/${projectRef}/functions/${slug}/body`, { token, fetcher }, async res => new Uint8Array(await res.arrayBuffer()));
+}
+
+// Delete a function
+// https://supabase.com/docs/reference/api/delete-a-function
+
+export async function deleteFunction({ projectRef, slug, token, fetcher }: { projectRef: string, slug: string, token: string, fetcher?: Fetcher }): Promise<ApiResponse<boolean>> {
+    return await execute(`/v1/projects/${projectRef}/functions/${slug}`, { token, fetcher, method: 'DELETE' }, res => res.status === 200);
+}
+
+// Create a function
+// https://supabase.com/docs/reference/api/create-a-function
+
+export type CreateFunctionOpts = { projectRef: string, slug: string, name: string, verify_jwt?: boolean, import_map?: boolean, entrypoint_path?: string, import_map_path?: string, brotliCompressedEszip: Uint8Array, token: string, fetcher?: Fetcher };
+
+export async function createFunction({ projectRef, slug, name, verify_jwt, import_map, entrypoint_path, import_map_path, brotliCompressedEszip, token, fetcher }: CreateFunctionOpts): Promise<ApiResponse<FunctionInfo>> {
+    return await execute(`/v1/projects/${projectRef}/functions`, { token, fetcher, method: 'POST', body: brotliCompressedEszip, bodyContentType: 'application/vnd.denoland.eszip', queryParams: { slug, name, verify_jwt, import_map, entrypoint_path, import_map_path } }, 
+        async res => checkFunctionInfo(await res.json()));
+}
+
+// Update a function
+// https://supabase.com/docs/reference/api/update-a-function
+
+export type UpdateFunctionOpts = { projectRef: string, slug: string, newSlug?: string, name?: string, verify_jwt?: boolean, import_map?: boolean, entrypoint_path?: string, import_map_path?: string, brotliCompressedEszip: Uint8Array, token: string, fetcher?: Fetcher };
+
+export async function updateFunction({ projectRef, slug, newSlug, name, verify_jwt, import_map, entrypoint_path, import_map_path,brotliCompressedEszip, token, fetcher }: UpdateFunctionOpts): Promise<ApiResponse<FunctionInfo>> {
+    return await execute(`/v1/projects/${projectRef}/functions/${slug}`, { token, fetcher, method: 'PATCH', body: brotliCompressedEszip, bodyContentType: 'application/vnd.denoland.eszip', queryParams: { slug: newSlug, name, verify_jwt, import_map, entrypoint_path, import_map_path } }, 
+        async res => checkFunctionInfo(await res.json()));
+}
+
+// Execute a function
+
+// https://supabase.com/docs/guides/functions/regional-invocation#available-regions
+export type Region = 'ap-northeast-1' | 'ap-northeast-2' | 'ap-south-1' | 'ap-southeast-1' | 'ap-southeast-2' | 'ca-central-1' | 'eu-central-1' | 'eu-west-1' | 'eu-west-2' | 'eu-west-3' | 'sa-east-1' | 'us-east-1' | 'us-west-1' | 'us-west-2';
+
+export async function executeFunction({ projectRef, slug, region, token, fetcher = fetch }: { projectRef: string, slug: string, region?: Region, token: string, fetcher?: Fetcher }): Promise<Response> {
+    return await fetcher(`https://${projectRef}.supabase.co/functions/v1/${slug}`, { headers: { authorization: `Bearer ${token}`, ...(region ? { 'x-region': region } : {}) } });
+}
+
+//
+
+export type ApiResponse<TResult> = {  meta: Record<string, string>, result: TResult };
+
+export type Fetcher = (url: string, opts: { method?: string, headers?: Record<string, string>, body?: Uint8Array }) => Promise<Response>;
+
+//
+
+// deno-lint-ignore no-explicit-any
+function isStringRecord(obj: any): obj is Record<string, unknown> {
+    return typeof obj === 'object' && obj !== null && !Array.isArray(obj) && obj.constructor === Object;
+}
+
+function checkArrayOf<T>(obj: unknown, checkFn: (obj: unknown) => T): readonly T[] {
+    if (!Array.isArray(obj)) throw new Error(JSON.stringify(obj));
+    return obj.map(checkFn);
+}
+
+type ExecuteOpts = { token: string, method?: 'GET' | 'DELETE' | 'POST' | 'PATCH', queryParams?: Record<string, string | boolean | undefined>, body?: Uint8Array, bodyContentType?: string, fetcher?: Fetcher };
+
+async function execute<TResult>(pathname: string, { token, method = 'GET', queryParams = {}, body, bodyContentType, fetcher = fetch }: ExecuteOpts, resultFn: (res: Response) => Promise<TResult> | TResult): Promise<ApiResponse<TResult>> {
+    const url = new URL(`https://api.supabase.com${pathname}`);
+    for (const [ name, value ] of Object.entries(queryParams)) {
+        if (value !== undefined) url.searchParams.set(name, value.toString());
+    }
+    const res = await fetcher(url.toString(), { method, headers: { authorization: `Bearer ${token}`, ...(bodyContentType ? { 'content-type': bodyContentType } : {}) }, body });
+    const expected = method === 'POST' ? 201 : 200;
+    if (res.status !== expected && !(method === 'DELETE' && res.status === 404)) throw new Error(`Expected ${expected}, found ${res.status} ${await res.text()}`);
+    const meta = Object.fromEntries(res.headers);
+    const result = await resultFn(res);
+    return { meta, result };
+}
