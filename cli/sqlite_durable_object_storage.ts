7f2e20fd77732f2f1e375968bea7356c81e92b8d
John Spurlock <john@skymethod.com>
2025-02-18 17:42:04 -0500

    serve: First-pass dosql kv storage, pass simple common test.

--- cli/sqlite_durable_object_storage.ts
+++ cli/sqlite_durable_object_storage.ts
@@ -2,7 +2,8 @@
 // import { DenoDir } from 'https://esm.sh/jsr/@deno/cache-dir@0.11.1';
 // import { DB, QueryParameter } from 'https://deno.land/x/sqlite@v3.8/mod.ts';
 // deno-lint-ignore no-explicit-any
-type DB = any;
+type DB = any; type QueryParameter = any;
+import { checkMatches, isStringRecord } from '../common/check.ts';
 import { DurableObjectGetAlarmOptions, DurableObjectId, DurableObjectSetAlarmOptions, DurableObjectStorage, DurableObjectStorageListOptions, DurableObjectStorageReadOptions, DurableObjectStorageTransaction, DurableObjectStorageValue, DurableObjectStorageWriteOptions, SqlStorage, SqlStorageCursor, SqlStorageValue } from '../common/cloudflare_workers_types.d.ts';
 import { InMemoryAlarms } from '../common/storage/in_memory_alarms.ts';
 import { join } from './deps_cli.ts';
@@ -20,10 +21,15 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
         this.db = db;
         this.alarms = new InMemoryAlarms(dispatchAlarm);
         this.sqlStorage = new SqliteSqlStorage(this);
+        this.init();
     }
 
     static async provider(className: string, id: DurableObjectId, options: Record<string, string>, dispatchAlarm: () => void, dbPathForInstance: ComputeDbPathForInstance = SqliteDurableObjectStorage.defaultDbPathForInstance) {
-        const dbPath = await dbPathForInstance({ container: options.container || 'default', className, id: id.toString() });
+        const dbPath = await dbPathForInstance({ 
+            container: checkMatches('container', options.container || 'default', DB_PATH_TOKEN), 
+            className: checkMatches('className', className, DB_PATH_TOKEN),
+            id: checkMatches('id', id.toString(), DB_PATH_TOKEN),
+        });
         console.log(`new SqliteDurableObjectStorage(${dbPath})`);
         const { DB } = await import('https://deno.land/x/sqlite@v3.8/mod.ts' + '');
         const db = new DB(dbPath);
@@ -49,7 +55,18 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
     }
 
     deleteAll(): Promise<void> {
-        throw new Error(`SqliteDurableObjectStorage.deleteAll() not implemented`);
+        const { db } = this;
+        const names = db.query(`select name from sqlite_master where type = 'table' order by name asc`) as [ string ][];
+        for (const [ name ] of names) {
+            const skip = name.startsWith('sqlite_');
+            if (skip) {
+                console.log(`deleteAll: skipping ${name}`);
+                continue;
+            }
+            db.execute(`drop table ${name}`);
+        }
+        this.init();
+        return Promise.resolve();
     }
 
     get(key: string, opts?: DurableObjectStorageReadOptions): Promise<DurableObjectStorageValue | undefined>;
@@ -59,6 +76,23 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
     }
 
     _get(keyOrKeys: string | readonly string[], opts: DurableObjectStorageReadOptions = {}): Promise<Map<string, DurableObjectStorageValue> | DurableObjectStorageValue | undefined> {
+        const { ...rest } = opts;
+        if (Object.keys(rest).length === 0) {
+            if (typeof keyOrKeys === 'string') {
+                const rows = this.db.query(`select type, value from ${KV_TABLE} where key = ?`, [ packKey(keyOrKeys) ]) as [ number, string ][];
+                for (const [ type, value ] of rows) {
+                    return Promise.resolve(unpackValue(type, value));
+                }
+                return Promise.resolve(undefined);
+            } else if (Array.isArray(keyOrKeys)) {
+                const rows = this.db.query(`select key, type, value from ${KV_TABLE} where key in (${keyOrKeys.map(_ => '?').join(', ')})`, keyOrKeys.map(packKey)) as [ Uint8Array, number, string ][];
+                const rt = new Map<string, DurableObjectStorageValue>();
+                for (const [ key, type, value ] of rows) {
+                    rt.set(unpackKey(key), unpackValue(type, value));
+                }
+                return Promise.resolve(rt);
+            }
+        }
         throw new Error(`SqliteDurableObjectStorage.get(${JSON.stringify({ keyOrKeys, opts })}) not implemented`);
     }
    
@@ -69,6 +103,20 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
     }
 
     _put(arg1: unknown, arg2?: unknown, arg3?: unknown): Promise<void> {
+        const { db } = this;
+        if (typeof arg1 === 'string') {
+            const [ type, packedValue ] = packValue(arg2);
+            db.query(`replace into ${KV_TABLE}(key, type, value) values (?, ?, ?)`, [ packKey(arg1), type, packedValue ]);
+            return Promise.resolve();
+        } else if (isStringRecord(arg1)) {
+            db.transaction(() => {
+                for (const [ key, value ] of Object.entries(arg1)) {
+                    const [ type, packedValue ] = packValue(value);
+                    db.query(`replace into ${KV_TABLE}(key, type, value) values (?, ?, ?)`, [ packKey(key), type, packedValue ]);
+                }
+            });
+            return Promise.resolve();
+        }
         throw new Error(`SqliteDurableObjectStorage.put(${JSON.stringify({ arg1, arg2, arg3 })}) not implemented`);
     }
    
@@ -78,12 +126,52 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
         return this._delete(keyOrKeys, opts);
     }
 
-    _delete(keyOrKeys: string | readonly string[], opts?: DurableObjectStorageWriteOptions): Promise<boolean | number> {
+    _delete(keyOrKeys: string | readonly string[], opts: DurableObjectStorageWriteOptions = {}): Promise<boolean | number> {
+        const { db } = this;
+        const { ...rest } = opts;
+        if (Object.keys(rest).length === 0) {
+            if (typeof keyOrKeys === 'string') {
+                const rows = db.query(`delete from ${KV_TABLE} where key = ? returning key`, [ packKey(keyOrKeys) ]);
+                return Promise.resolve(rows.length > 0);
+            } else if (Array.isArray(keyOrKeys)) {
+                const rows = db.query(`delete from ${KV_TABLE} where key in (${keyOrKeys.map(_ => '?').join(', ')}) returning key`, keyOrKeys.map(packKey));
+                return Promise.resolve(rows.length);
+            }
+        }
         throw new Error(`SqliteDurableObjectStorage.delete(${JSON.stringify({ keyOrKeys, opts })}) not implemented`);
     }
    
-    async list(options: DurableObjectStorageListOptions & DurableObjectStorageReadOptions = {}): Promise<Map<string, DurableObjectStorageValue>> {
-        throw new Error(`SqliteDurableObjectStorage.list(${JSON.stringify({ options })}) not implemented`);
+    list(options: DurableObjectStorageListOptions & DurableObjectStorageReadOptions = {}): Promise<Map<string, DurableObjectStorageValue>> {
+        const { limit, reverse, start, startAfter, end, prefix, ...rest } = options;
+        if (Object.keys(rest).length > 0) throw new Error(`SqliteDurableObjectStorage.list(${JSON.stringify({ options })}) not implemented`);
+
+        const whereClauses: string[] = [];
+        const params: QueryParameter[] = [];
+        if (typeof start === 'string') {
+            whereClauses.push(`key ${reverse ? '<=' : '>='} ?`);
+            params.push(packKey(start));
+        }
+        if (typeof startAfter === 'string') {
+            whereClauses.push(`key ${reverse ? '<' : '>'} ?`);
+            params.push(packKey(startAfter));
+        }
+        if (typeof end === 'string') {
+            whereClauses.push(`key ${reverse ? '>' : '<'} ?`);
+            params.push(packKey(end));
+        }
+        if (typeof prefix === 'string' && prefix.length > 0) {
+            const prefixBytes = packKey(prefix);
+            whereClauses.push(`substr(key, 1, ${prefixBytes.length}) = ?`);
+            params.push(prefixBytes);
+        }
+
+        const q = `select key, type, value from ${KV_TABLE}${whereClauses.length > 0 ? ` where ${whereClauses.join(' and ')}` : ''} order by key ${reverse ? 'desc' : 'asc'}${typeof limit === 'number' ? ` limit ${limit}` : ''}`;
+        const rows = this.db.query(q, params) as [ Uint8Array, number, string][];
+        const rt = new Map<string, DurableObjectStorageValue>();
+        for (const [ keyBlob, type, valueBlob ] of rows) {
+            rt.set(unpackKey(keyBlob), unpackValue(type, valueBlob));
+        }
+        return Promise.resolve(rt);
     }
 
     getAlarm(options?: DurableObjectGetAlarmOptions): Promise<number | null> {
@@ -118,6 +206,55 @@ export class SqliteDurableObjectStorage implements DurableObjectStorage {
         return this.sqlStorage;
     }
 
+    //
+
+    private init() {
+        this.db.execute(`create table if not exists ${KV_TABLE}(key blob primary key, type integer not null, value text not null) without rowid`);
+    }
+
+}
+
+//
+
+const DB_PATH_TOKEN = /^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$/;
+const KV_TABLE = `_cf_KV`;
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+function unpackKey(packed: Uint8Array): string {
+    return decoder.decode(packed);
+}
+
+function packKey(key: string): Uint8Array {
+    return encoder.encode(key);
+}
+
+function unpackValue(type: number, packed: string): DurableObjectStorageValue {
+    if (type === 1) return undefined as unknown as DurableObjectStorageValue;
+    if (type === 2) return null as unknown as DurableObjectStorageValue;
+    if (type === 3) return packed;
+    if (type === 4) return parseInt(packed);
+    if (type === 5) return JSON.parse(packed);
+    throw new Error(`unpackValue: Unsupported type: ${type}`);
+}
+
+function packValue(value: unknown): [ number, string ] {
+    if (value === undefined) return [ 1, '' ];
+    if (value === null) return [ 2, '' ];
+    if (typeof value === 'string') return [ 3, value ];
+    if (typeof value === 'number') {
+        if (Number.isSafeInteger(value)) return [ 4, value.toString() ];
+    }
+    if (typeof value === 'object' && isSafeJson(value)) return [ 5, JSON.stringify(value) ];
+    throw new Error(`packValue: Unsupported value: ${typeof value} ${value}${typeof value === 'object' ? ` ${value.constructor.name}` : ''}`);
+}
+
+function isSafeJson(value: unknown): boolean {
+    if (value === null || typeof value === 'string' || typeof value === 'boolean') return true;
+    if (typeof value === 'number') return Number.isSafeInteger(value);
+    if (typeof value === 'object') return isStringRecord(value) && Object.values(value).every(isSafeJson);
+    return false;
 }
 
 //
@@ -185,7 +322,7 @@ class SqliteSqlStorage implements SqlStorage {
     }
 
     get databaseSize(): number {
-        const [ [ size ] ] = this.storage.db.query(`select page_count * page_size from pragma_page_count(), pragma_page_size()`);
+        const [ [ size ] ] = this.storage.db.query(`select page_count * page_size from pragma_page_count(), pragma_page_size()`) as [ number ][];
         return size;
     }
 
---
+++ cli/sqlite_durable_object_storage_test.ts
@@ -0,0 +1,11 @@
+import { runSimpleStorageTestScenario } from '../common/storage/durable_object_storage_test.ts';
+import { SqliteDurableObjectStorage } from './sqlite_durable_object_storage.ts';
+
+Deno.test({
+    name: 'SqliteDurableObjectStorage',
+    ignore: (await Deno.permissions.query({ name: 'net' })).state !== 'granted',
+    async fn() {
+        const storage = await SqliteDurableObjectStorage.provider('class1', 'id1', {}, () => {}, () => ':memory:');
+        await runSimpleStorageTestScenario(storage);
+    }
+});
